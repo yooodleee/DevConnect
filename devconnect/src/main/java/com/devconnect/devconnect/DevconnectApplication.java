ea782c15d55fe2063fd5de4d6145abc95cac057a
yooodleee <accia25@naver.com>
2025-07-06 00:46:10 +0900

    init project

---
+++ devconnect/src/main/java/com/devconnect/devconnect/DevconnectApplication.java
@@ -0,0 +1,13 @@
+package com.devconnect.devconnect;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevconnectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DevconnectApplication.class, args);
+    }
+
+}
---
+++ devconnect/src/test/java/com/devconnect/devconnect/DevconnectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devconnect.devconnect;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevconnectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
